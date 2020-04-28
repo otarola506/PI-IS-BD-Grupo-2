349496d3d3056c673cdff43846e050fa3f6edbef
Ignacio <joseignacioviquezrojas@gmail.com>
2020-04-27 18:16:56 -0600

    Proyecto vacio para editar

---
+++ Iteracion_1/Iteracion_1.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Iteracion_1.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Iteracion_1/Iteracion_1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Iteracion_1.Global" Language="C#" %>
