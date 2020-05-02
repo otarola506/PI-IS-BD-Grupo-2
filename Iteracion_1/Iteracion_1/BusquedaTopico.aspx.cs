7a72374624d2a2860ca67eb7296a5ab3077a988d
Dasc1227 <davidsolco@gmail.com>
2020-05-01 18:54:17 -0600

    Cambios la busqueda, ahora funciona la busqueda por titulo, solo falta redireccionarlo al codigo de desplegar

--- Iteracion_1/Iteracion_1/BusquedaTopico.aspx.cs
+++ Iteracion_1/Iteracion_1/BusquedaTopico.aspx.cs
@@ -6,11 +6,19 @@ using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.Data.SqlClient;
 using System.Data;
+using System.Configuration;
 
 namespace Iteracion_1
 {
     public partial class BusquedaTopico : System.Web.UI.Page
     {
+
+        private SqlConnection con;
+        private void connection()
+        {
+            string conString = ConfigurationManager.ConnectionStrings["grupo2Conn"].ToString();
+            con = new SqlConnection(conString);
+        }
         protected void Page_Load(object sender, EventArgs e)
         {
             if (!IsPostBack)
@@ -25,19 +33,39 @@ namespace Iteracion_1
             // EN UserID ponen el usuario que usan para entrar a la base de datos
             // En password usan la clave que usan para entrar a la base de datos
             //Para ejecutar la vara usan CTRL + F5
-
-            SqlConnection cn = new SqlConnection("Data Source=172.16.202.24,1433;Network Library=DBMSSOCN;Initial Catalog=BD_Grupo2;User ID=Grupo2;Password=grupo2.");
+            connection();
+            con.Open();
+            //SqlConnection cn = new SqlConnection("Data Source=172.16.202.24,1433;Network Library=DBMSSOCN;Initial Catalog=BD_Grupo2;User ID=Grupo2;Password=grupo2.");
             // Ahorita el comando solo pregunta por el ID pero puede preguntar por otras varas más si lo modifican
-            SqlCommand cmd = new SqlCommand("SELECT A.* FROM Articulo A JOIN Art_Categoria AC ON A.artIdPK = AC.artIdFK JOIN Categoria C ON AC.categoriaIdFK = C.categoriaIdPK WHERE  C.nombre ="+ "'"+ DropDownList1.SelectedValue + "'", cn);
+            SqlCommand cmd = new SqlCommand("SELECT A.* FROM Articulo A JOIN Art_Categoria AC ON A.artIdPK = AC.artIdFK JOIN Categoria C ON AC.categoriaIdFK = C.categoriaIdPK WHERE  C.nombre ="+ "'"+ DropDownList1.SelectedValue + "'", con);
             SqlDataAdapter ad = new SqlDataAdapter(cmd);
             DataTable dt = new DataTable();
             ad.Fill(dt);
             tabla.DataSource = dt;
             tabla.DataBind();
-            cn.Close();
+            con.Close();
             Response.Write("Datos Cargados Correctamente");
            // Session["param"] = DropDownList1.SelectedValue;
             //Response.Redirect("Display.aspx");
+        }
+
+        protected void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void BusquedaXTitulo_Click(object sender, EventArgs e)
+        {
+            connection();
+            con.Open();
+            SqlCommand cmd = new SqlCommand("SELECT * FROM Articulo WHERE titulo LIKE " + "'%" + TextBox1.Text + "%'", con);
+            SqlDataAdapter ad = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            ad.Fill(dt);
+            tabla.DataSource = dt;
+            tabla.DataBind();
+            con.Close();
+            Response.Write("Datos Cargados Correctamente");
         }
     }
 }
--- Iteracion_1/Iteracion_1/BusquedaTopico.aspx.designer.cs
+++ Iteracion_1/Iteracion_1/BusquedaTopico.aspx.designer.cs
@@ -31,15 +31,6 @@ namespace Iteracion_1 {
         protected global::System.Web.UI.WebControls.DropDownList DropDownList1;
         
         /// <summary>
-        /// SqlDataSource1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.SqlDataSource SqlDataSource1;
-        
-        /// <summary>
         /// Btn_ConsultaXTopico control.
         /// </summary>
         /// <remarks>
@@ -56,5 +47,32 @@ namespace Iteracion_1 {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.GridView tabla;
+        
+        /// <summary>
+        /// SqlDataSource1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.SqlDataSource SqlDataSource1;
+        
+        /// <summary>
+        /// TextBox1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox1;
+        
+        /// <summary>
+        /// BusquedaXTitulo control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button BusquedaXTitulo;
     }
 }
