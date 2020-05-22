fd17612fed22167b98723ab61bfe388fb9524035
Sebastian Otarola <otarolasebas@hotmail.com>
2020-05-22 14:03:44 -0600

    Se crea procedimiento almacenado para consultar correos, esto sirve para el dropdown list que hay que implementar en el correo

--- Iteracion_2/Iteracion_2/Models/Email.cs
+++ Iteracion_2/Iteracion_2/Models/Email.cs
@@ -1,14 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
+
 
 namespace Iteracion_2.Models
 {
     public class Email
     {
+        private SqlConnection con;
+        private void connection()
+        {
+            string conString = ConfigurationManager.ConnectionStrings["conexionECCI"].ToString();
+            con = new SqlConnection(conString);
+        }
         public string To { get; set; }
         public string Subject { get; set; }
         public string Body { get; set; }
+
+        // Esto se hace en el model perfil o miembro para recuperar correos por el momento esto es solo para pruebas
+        public IEnumerable<string> recuperarCorreos() {
+            SqlCommand cmd = new SqlCommand("Recuperar_Correos", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            List <string> results = new List<string>();
+            using (SqlDataReader reader = cmd.ExecuteReader())
+            {
+                while (reader.Read())
+                    results.Add(reader["correo"].ToString());
+            }
+            return results;
+
+        }
     }
 }
