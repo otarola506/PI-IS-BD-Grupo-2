9c460cdd1fce0b930cc4401ad1a6fb99e1e2e25d
IvanCh123 <40545827+IvanCh123@users.noreply.github.com>
2020-05-21 22:11:55 -0600

    Actualización de tablas de BC, segunda iteración

--- SQL/creacionDeTablas.sql
+++ SQL/creacionDeTablas.sql
@@ -1,40 +1,83 @@
 USE BD_Grupo2;
+
 CREATE TABLE Miembro(
-	miembroIdPK INTEGER NOT NULL PRIMARY KEY IDENTITY (-1,1),
+	miembroIdPK INTEGER NOT NULL PRIMARY KEY IDENTITY (1,1),
 	nombre VARCHAR(50) NOT NULL,
 	pesoMiembro INTEGER,
-	correo VARCHAR(50)
+	nombreUsuario VARCHAR(50)
 );
+
+CREATE TABLE Nucleo(
+	miembroIdFK INTEGER NOT NULL PRIMARY KEY,
+	tipo INTEGER NOT NULL,
+
+	CONSTRAINT FK_Nucleo_Miembro
+		FOREIGN KEY(miembroIdFK) REFERENCES Miembro(miembroIdPK)
+			ON DELETE CASCADE
+);
+
+CREATE TABLE Perfil(
+	perfilIdPK INTEGER NOT NULL IDENTITY (1,1),
+	miembroIdFK INTEGER NOT NULL,
+	informacionLaboral VARCHAR(MAX),
+	informacionBiografica VARCHAR(MAX),
+	telefono VARCHAR(20),
+	correo VARCHAR(20),
+	merito FLOAT
+
+	CONSTRAINT PK_PerfilMiembro PRIMARY KEY(perfilIdPK,miembroIdFK)
+
+	CONSTRAINT FK_Perfil_Miembro
+		FOREIGN KEY(miembroIdFK) REFERENCES Miembro(miembroIdPK)
+			ON DELETE CASCADE
+);
+
+
 CREATE TABLE Articulo(
 	artIdPK INTEGER NOT NULL PRIMARY KEY IDENTITY (1,1),
 	titulo VARCHAR(max) NOT NULL,
 	resumen VARBINARY(max) NOT NULL,
 	contenido VARBINARY(max) NOT NULL,
-	tipoArt bit NOT NULL,
-	extn VARCHAR(15),
+	puntuacion INTEGER,
+	visitas INTEGER,
 	estado INTEGER,
-	puntuacion INTEGER
+	tipoArt bit NOT NULL
+
 ); 
 
+CREATE TABLE Miembro_Articulo(
+	miembroIdFK INTEGER NOT NULL,
+	artIdFK INTEGER NOT NULL, 
+
+	CONSTRAINT PK_ArticuloMiembro PRIMARY KEY(miembroIdFK,artIdFK),
+	CONSTRAINT FK_Escribe_Miembro
+		FOREIGN KEY(miembroIdFK) REFERENCES Miembro(miembroIdPK)
+			ON DELETE CASCADE,
+	CONSTRAINT FK_Escribe_Articulo
+		FOREIGN KEY(artIdFK) REFERENCES Articulo(artIdPK)
+			ON DELETE CASCADE
+);
+
 CREATE TABLE Categoria(
-	categoriaIdPK INTEGER NOT NULL PRIMARY KEY IDENTITY (-1,1),
+	categoriaIdPK INTEGER NOT NULL PRIMARY KEY IDENTITY (1,1),
 	nombre VARCHAR(20) NOT NULL, 
 );
 
 CREATE TABLE Art_Categoria (
-	categoriaIdFK INTEGER NOT NULL DEFAULT -1,
+	categoriaIdFK INTEGER NOT NULL,
 	artIdFK INTEGER NOT NULL
 	
 	CONSTRAINT PK_ArticuloCategoria PRIMARY KEY(categoriaIdFK,artIdFK)
 
 	CONSTRAINT FK_Art_Categoria_Categoria
 	FOREIGN KEY(categoriaIdFK) REFERENCES Categoria (categoriaIdPK)
-		ON DELETE SET DEFAULT,
+		ON DELETE CASCADE,
 
 	CONSTRAINT FK_Art_Categoria_Articulo
 	FOREIGN KEY(artIdFK) REFERENCES Articulo (artIdPK)
 		ON DELETE CASCADE
 );
+
 CREATE TABLE Pregunta_Frecuente(
 	pregIdPK INTEGER NOT NULL PRIMARY KEY IDENTITY (1,1),
 	miembroIdFK INTEGER DEFAULT -1,
@@ -45,32 +88,4 @@ CREATE TABLE Pregunta_Frecuente(
 		FOREIGN KEY(miembroIdFK) REFERENCES Miembro(miembroIdPK)
 			ON DELETE SET DEFAULT
 );
-CREATE TABLE Miembro_Nucleo(
-	miembroIdFK INTEGER  NOT NULL DEFAULT -1,
-	
-	CONSTRAINT FK_Nucleo_Miembro
-		FOREIGN KEY(miembroIdFK) REFERENCES Miembro(miembroIdPK)
-			ON DELETE SET DEFAULT
-);
 
-CREATE TABLE Miembro_Articulo(
-	miembroIdFK INTEGER NOT NULL DEFAULT -1,
-	artIdFK INTEGER NOT NULL, 
-
-	CONSTRAINT PK_ArticuloMiembro PRIMARY KEY(miembroIdFK,artIdFK),
-	CONSTRAINT FK_Escribe_Miembro
-		FOREIGN KEY(miembroIdFK) REFERENCES Miembro(miembroIdPK)
-			ON DELETE SET DEFAULT,
-	CONSTRAINT FK_Escribe_Articulo
-		FOREIGN KEY(artIdFK) REFERENCES Articulo(artIdPK)
-			ON DELETE CASCADE
-);
-
-
-
-
-INSERT INTO Miembro
-VALUES('Anonimo')
-
-INSERT INTO Categoria
-VALUES('No especificada')
