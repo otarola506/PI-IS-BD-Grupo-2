43dc22a81b46f85b4990cb0fc01508407ae1036b
Ignacio <joseignacioviquezrojas@gmail.com>
2020-05-23 00:48:58 -0600

    Implementacion de la historia #9, faltan pruebas mas a fondo.

---
+++ Iteracion_2/Iteracion_2/Controllers/controladorMeritos.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Iteracion_2.Models;
+
+namespace Iteracion_2.Controllers
+{
+    public class controladorMeritos
+    {
+        //tenemos que llamar a los modelos 
+        ConexionBD conexion { set; get; }
+
+
+        //Agarro lo que me manda la vista y lo paso al modelo
+        //Despues tengo que mandarle ese valor a la vista
+        public string revisarEstadoArticulo(int artID)
+        {
+            string valor_a_vista = "";
+            //Lamamos para que se cree la conexion
+            conexion = new ConexionBD();
+            conexion.connection();
+            valor_a_vista = conexion.revisarEstadoArticulo(artID);
+            return valor_a_vista;
+        }
+
+
+        public string asignarPuntajeInicial(int artID)
+        {
+            string idAutor_a_vista = "";
+            conexion = new ConexionBD();
+            conexion.connection();
+            conexion.asignarMeritoPuntuacionInicial(artID);
+
+            return idAutor_a_vista;
+        }
+
+
+    }
+}
--- Iteracion_2/Iteracion_2/Models/ConexionBD.cs
+++ Iteracion_2/Iteracion_2/Models/ConexionBD.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Configuration;
 using System.Data;
-
+using System.Globalization;
 
 namespace Iteracion_2.Models
 {
@@ -16,12 +16,8 @@ namespace Iteracion_2.Models
         public void connection()
         {
             string conString = @"Server=172.16.202.75;Database=BD_B78451;persist security info=True;MultipleActiveResultSets=True;User ID=B78451;Password=contra";
-
             con = new SqlConnection(conString);
-            //con.Open();
-            //SqlCommand prueba = new SqlCommand("SELECT * FROM TablaPrueba", con);
 
-            //con.Close();
         }
 
 
@@ -33,7 +29,7 @@ namespace Iteracion_2.Models
             string valor = "";
             SqlCommand cmd = new SqlCommand("RecuperarEstadoArticulo", con);
             cmd.CommandType = CommandType.StoredProcedure;
-            cmd.Parameters.Add("@artID", SqlDbType.VarChar).Value = artID; // le pasamos el artID
+            cmd.Parameters.Add("@artID", SqlDbType.Int).Value = artID; // le pasamos el artID
             SqlDataReader reader = cmd.ExecuteReader();
             if (reader.Read())
             {
@@ -43,6 +39,74 @@ namespace Iteracion_2.Models
 
             con.Close();
             return valor;
+        }
+
+        public string retornarAutoresControlador(int artId, float puntuacion)
+        {
+            con.Open();
+            string miembroID = "";
+            SqlCommand cmd2 = new SqlCommand("RecuperarAutores", con);
+            cmd2.CommandType = CommandType.StoredProcedure;
+            cmd2.Parameters.Add("@artID", SqlDbType.Int).Value = artId; // le pasamos el valor del string
+            SqlDataReader reader = cmd2.ExecuteReader();
+            while (reader.Read())
+            {
+                miembroID = reader[0].ToString(); // para cada autor ocupo que guarde el merito en puntaje
+                asignarPuntajeInicial(miembroID, puntuacion);
+            }
+            reader.Close();
+            con.Close();
+            return miembroID;
+        }
+
+        //Metodo que se encarga de asignarle el puntaje inicial al autor que se le pasa por parametro
+        public void asignarPuntajeInicial(string miembroID, float puntuacion)
+        {
+            //Se llama al proc que guarda el puntaje en el perfil de la persona, en este momento se guarda en una base de prueba 
+            //con.Open();
+            SqlCommand cmd = new SqlCommand("ingresarPuntuacionAutor", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.Add("@puntuacion", SqlDbType.Float).Value = puntuacion;
+            cmd.Parameters.Add("@miembroID", SqlDbType.Int).Value = miembroID;
+            cmd.ExecuteNonQuery();
+            //con.Close();
+
+
+        }
+
+        //Metodo que primero revisa si el artID esta aprobado, luego obtiene la puntuacion del articulo y lo asigna
+        public void asignarMeritoPuntuacionInicial(int artID)
+        {
+            string aceptado = revisarEstadoArticulo(artID);
+            if (aceptado == "1")
+            {
+                //Obtenemos la puntacion del articulo
+                con.Open();
+                string puntuacion = "";
+                SqlCommand cmd = new SqlCommand("RecuperarPuntuacionArticulo", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.Add("@artID", SqlDbType.Int).Value = artID;
+                SqlDataReader reader = cmd.ExecuteReader();
+                if (reader.Read())
+                {
+                    puntuacion = reader[0].ToString();
+                }
+
+                reader.Close();
+                con.Close();
+
+                float valor = (float)Convert.ToDouble(puntuacion);
+
+                //obtemos los autores y asignamos 
+                retornarAutoresControlador(artID, valor);
+
+
+                int a = 8;
+
+            }
+
+
+
         }
 
 
