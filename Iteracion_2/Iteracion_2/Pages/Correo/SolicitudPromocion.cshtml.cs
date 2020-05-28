3a109536ac61b37965116537d4999e08af0d5719
Dasc1227 <davidsolco@gmail.com>
2020-05-28 13:33:31 -0600

    Se inicia la implementacion del diseño del formulario de la solicitud

---
+++ Iteracion_2/Iteracion_2/Pages/Correo/SolicitudPromocion.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Iteracion_2.Controllers;
+
+namespace Iteracion_2.Pages.Correo
+{
+    public class SolicitudPromocionModel : PageModel
+    {
+        public EmailController controlador { get; set; }
+        public async Task<IActionResult> OnPost()
+        {
+            controlador = new EmailController();
+            string destinatarioCorreo = Request.Form["destinatario"];
+
+            string asunto = Request.Form["asunto"];
+            string contenido = Request.Form["contenido"];
+            //await controlador.enviarCorreo(destinatarioCorreo, asunto, contenido, archivo);
+
+            TempData["resultado"] = "El correo ha sido enviado a " + destinatarioCorreo;
+            return new RedirectToPageResult("Correo");
+        }
+        public void OnGet()
+        {
+
+        }
+    }
+}
