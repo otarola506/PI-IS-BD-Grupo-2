53fc6773ae89d7c07777bfef1967de64a5ef4e2e
Ignacio <joseignacioviquezrojas@gmail.com>
2020-05-30 12:18:20 -0600

    Redireccion de los proyectos

---
+++ Iteracion_2/Iteracion_2/Pages/Articulos/MisArticulos.cshtml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Iteracion_2.Pages.Articulos
+{
+    public class MisArchivosModel : PageModel
+    {
+        private const string Location = "http://localhost:51359/MisArticulos";
+        Encoding unicode = Encoding.Unicode;
+        public void OnGet()
+        {
+            Response.Redirect(Location);
+            
+        }
+    }
+}
---
+++ Iteracion_2/Iteracion_2/Pages/Articulos/TodosArticulos.cshtml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Iteracion_2.Pages.Articulos
+{
+    public class TodosArticulosModel : PageModel
+    {
+        private const string Location = "http://localhost:51359/Articulos";
+        public void OnGet()
+        {
+            Response.Redirect(Location);
+        }
+    }
+}
