20b01d39ee9d3405c4becbbb4c55bf0ebc2e94bd
Ignacio <joseignacioviquezrojas@gmail.com>
2020-05-30 20:07:21 -0600

    Creacion de vista de registro

---
+++ Iteracion_2/Iteracion_2/Pages/Registrar/Ingresar.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Iteracion_2.Pages.Registrar
+{
+    public class IngresarModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
