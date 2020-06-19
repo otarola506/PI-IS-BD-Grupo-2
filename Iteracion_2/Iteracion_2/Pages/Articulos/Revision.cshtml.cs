4719747d565facc049f45fffd749276f3ee4680e
Sebastian Otarola <otarolasebas@hotmail.com>
2020-06-18 22:32:01 -0600

    [Tarea # 715]
    [Módulo Revisión Artículos]
    
    [Primer intento para captura de artId y titiulo del artículo en vista articulos pendientes de revisión]

--- Iteracion_2/Iteracion_2/Pages/Articulos/Revision.cshtml.cs
+++ Iteracion_2/Iteracion_2/Pages/Articulos/Revision.cshtml.cs
@@ -14,10 +14,15 @@ namespace Iteracion_2.Pages.Articulos
         const string SessionKeyUsuario = "UsuarioActual";
         const string SessionKeyPeso = "PesoActual";
         private ArticuloController ArticuloController { get; set; }
+        
 
         public List<List<string>> ArticulosPendientes { get; set; }
 
         public string UsuarioActual { get; set; }
+
+        public int ArtIDPk { get; set;}
+
+        public string Titulo { get; set; }
         public IActionResult OnGet()
         {
             UsuarioActual = HttpContext.Session.GetString(SessionKeyUsuario);
@@ -34,8 +39,17 @@ namespace Iteracion_2.Pages.Articulos
                 return RedirectToPage("/Cuenta/Ingresar", new {Mensaje = "Permisos insuficientes" });
             }
         }
+
+        public IActionResult OnPost()
+        {
+
+            int ArtId = ArtIDPk;
+            string TituloArt = Titulo;
+            return Page();
+        }
         // Método para que solo aparezca el botón para el coordinador.
         //Metódo onPost que pasa datos de artículo al controlador para que este se los pase al modelo de correo.
+
 
     }
 }
