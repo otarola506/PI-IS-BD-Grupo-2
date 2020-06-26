82da11bba43dd88dc0c23b17acd5e504464bab50
Ignacio <joseignacioviquezrojas@gmail.com>
2020-06-26 13:16:06 -0600

    [Refactoring] [Modelo Reporte Configurable] [Modificacion de las opciones para la consulta avanzada (SQL) para el grafico por varias categorias]

--- Iteracion_2/Iteracion_2/Models/DistribucionMiembroModel.cs
+++ Iteracion_2/Iteracion_2/Models/DistribucionMiembroModel.cs
@@ -36,46 +36,16 @@ namespace Iteracion_2.Models
             Connection();
             if (Valores.Length > 1)
             {
-                //Busqueda por mas de un valor
-                // hacemos la consulta avanzada  para los nombres que queremos en la grafica
-                //string AtributoDistribucion = "M." + Valores[0] + ", M."+Valores[1]+"";
-                //string QueryString = "SELECT @Valor1, COUNT(@Valor1), @Valor2, COUNT(@Valor2) FROM Miembro GROUP BY "+ AtributoDistribucion + "";
-                //using (SqlCommand command = new SqlCommand(QueryString, con))
-                //{
-
-                //    command.Parameters.AddWithValue("@Valor1", Valores[0]);
-                //    command.Parameters.AddWithValue("@Valor2", Valores[1]);
-                //    SqlDataReader reader = command.ExecuteReader();
-                //    while (reader.Read())
-                //    {
-                //        var reporte = new ReporteConfigurable { Entrada = reader[0].ToString(), Cantidad = reader[1].ToString(), Entrada2 = reader[2].ToString(), Cantidad2 = reader[3].ToString() };
-                //        Retorno.Add(reporte);
-                //    }
-
-                //}
-
-
-                string AtributoDistribucion = "M." + Valores[0] + ", M."+Valores[1]+"";
-                string QueryString = "SELECT @Valor1, COUNT(@Valor1), @Valor2, COUNT(@Valor2) FROM Miembro M GROUP BY " + AtributoDistribucion + "";
-                SqlCommand command = new SqlCommand(QueryString, con)
+                // Busqueda por varios valores 
+                string AtributoDistribucion = "M." + Valores[0] + "";
+                string AtributoDistribucionAvanzada = "M."+Valores[1]+ "";
+                SqlCommand command = new SqlCommand("SELECT " + AtributoDistribucion + ", COUNT(*)   FROM Miembro WHERE "+AtributoDistribucionAvanzada+" = '"+Valores[2]+"'   M GROUP BY " + AtributoDistribucion + "", con);
+                SqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
                 {
-                    CommandType = CommandType.Text
-                };
-
-                command.Parameters.AddWithValue("@Valor1", Valores[0]);
-                command.Parameters.AddWithValue("@Valor2", Valores[1]);
-
-                using (SqlDataReader reader = command.ExecuteReader())
-                {
-                    while (reader.Read())
-                    {
-                        var reporte = new ReporteConfigurable { Entrada = reader[0].ToString(), Cantidad = reader[1].ToString(), Entrada2 = reader[2].ToString(), Cantidad2 = reader[3].ToString() };
-                        Retorno.Add(reporte);
-                    }
-
+                    var reporte = new ReporteConfigurable { Entrada = reader[0].ToString(), Cantidad = reader[1].ToString() };
+                    Retorno.Add(reporte);
                 }
-
-
 
             }
             else if (Valores.Length == 1) //Busqueda por un unico valor
